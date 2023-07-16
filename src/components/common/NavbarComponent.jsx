7b661a92d5fc799b076b76b0f19c040223ea89c1
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-16 18:51:49 -0300

    Agregada la hoja de estilos del navbar

--- src/components/common/NavbarComponent.jsx
+++ src/components/common/NavbarComponent.jsx
@@ -1,12 +1,13 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import Logo from "../../assets/rollingVetLogo.svg";
+import "../../css/navbar.css";
 
 const NavbarPage = () => {
     return (
-        <Navbar expand="lg" style={{ backgroundColor: "#ddd" }}>
+        <Navbar expand="lg" className="nav">
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={Logo} alt="Rolling Vet logo oficial" style={{width:"150px"}} />
+                    <img src={Logo} alt="Rolling Vet logo oficial" className="logo"/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
