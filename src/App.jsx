d4eb3c44addf4fc71f0dc69a921e3f3254339132
Nicolas Herrera <herreranicoo17@gmail.com>
2023-07-18 18:39:56 -0300

    Agregue maquetado inicial para editar turno

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import AdminTurnos from "./components/views/AdminTurnos";
+import EditarTurno from "./components/views/turnos/EditarTurno";
 
 function App() {
   return (
     <>
-      <AdminTurnos />
+      {/* <AdminTurnos /> */}
+
+      <EditarTurno />
     </>
   );
 }
