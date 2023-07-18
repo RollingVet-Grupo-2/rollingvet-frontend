1c5ffde0b9a92c5eb8ad143962c13077dd49f9f4
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-18 03:09:30 -0300

    Agregado drowdown administrar

--- src/components/common/NavbarComponent.jsx
+++ src/components/common/NavbarComponent.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
 import Logo from "../../assets/rollingVetLogo.svg";
 import "../../css/navbar.css";
 
@@ -7,15 +7,34 @@ const NavbarPage = () => {
         <Navbar expand="lg" className="nav">
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={Logo} alt="Rolling Vet logo oficial" className="logo"/>
+                    <img
+                        src={Logo}
+                        alt="Rolling Vet logo oficial"
+                        className="logo"
+                    />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="#home">Inicio</Nav.Link>
-                        <Nav.Link href="#home">Administrar Turnos</Nav.Link>
-                        <Nav.Link href="#home">Administrar Pacientes</Nav.Link>
                         <Nav.Link href="#link">Login</Nav.Link>
+                        <Dropdown>
+                            <Dropdown.Toggle
+                                variant="danger"
+                                id="dropdown-basic"
+                            >
+                                Administrar
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu variant="dark">
+                                <Dropdown.Item href="#/action-1">
+                                    Administrar pacientes
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">
+                                    Administrar turnos
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
