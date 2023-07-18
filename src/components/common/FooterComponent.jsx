5cc12461d8660f7cfb67d2ac2cbedf71489f4613
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-18 02:45:14 -0300

    Añadiendo animaciones a los links del footer

--- src/components/common/FooterComponent.jsx
+++ src/components/common/FooterComponent.jsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/rollingVetLogo.svg";
 const FooterComponent = () => {
     return (
         <footer className="footer">
-            <section className="container py-4">
+            <section className="container py-4 px-0">
                 <div className="row mx-0 gap-4 gap-md-0">
                     <article className="col-md-4">
                         <img
@@ -13,43 +13,42 @@ const FooterComponent = () => {
                         />
                         <div className="d-flex gap-2">
                             <div>
-                                <i className="bi bi-facebook icons"></i>
+                                <i className="bi bi-facebook icons fs-4"></i>
                             </div>
                             <div>
-                                <i className="bi bi-instagram icons"></i>
+                                <i className="bi bi-instagram icons fs-4"></i>
                             </div>
                             <div>
-                                <i className="bi bi-whatsapp icons"></i>
+                                <i className="bi bi-whatsapp icons fs-4"></i>
                             </div>
                         </div>
                     </article>
                     <article className="col-md-4">
-                        <h3 className="footerSectionTitle">NOSOTROS</h3>
+                        <h3 className="footerSectionTitle fs-4">NOSOTROS</h3>
                         <div className="d-flex flex-column">
-                            <a href="#" className="links_footer">
+                            <a href="#" className="links_footer nav-link">
                                 Quiénes somos
                             </a>
-                            <a href="#" className="links_footer">
+                            <a href="#" className="links_footer nav-link">
                                 Servicios
                             </a>
-                            <a href="#" className="links_footer">
+                            <a href="#" className="links_footer nav-link">
                                 Medios de pago
                             </a>
                         </div>
                     </article>
                     <article className="col-md-4">
-                        <h3 className="footerSectionTitle">CONTACTANOS</h3>
+                        <h3 className="footerSectionTitle fs-4">CONTACTANOS</h3>
                         <div className="d-flex flex-column">
-                            <span className="text_contactanos_footer pb-0">Gral. Paz 576 4000 San Miguel de Tucumán</span>
-                            <span className="text_contactanos_footer">lunea a Sábado 8:30 a 13h | 16 a 21h</span>
-                            <span className="text_contactanos_footer">381 000 0000</span>
-                            <span className="text_contactanos_footer"></span>
+                            <span className="links_footer fs-6 pb-0">Gral. Paz 576 4000 San Miguel de Tucumán</span>
+                            <span className="links_footer fs-6">lunea a Sábado 8:30 a 13h | 16 a 21h</span>
+                            <span className="links_footer fs-6">381 000 0000</span>
                         </div>
                     </article>
                 </div>
             </section>
             <section className="d-flex justify-content-center align-items-center py-3 copy_footer_section">
-                <h3 className="fs-5 fw-light">&copy;Todos los derechos reservados - RollingVet</h3>
+                <h3 className="fs-6">&copy;Todos los derechos reservados - RollingVet</h3>
             </section>
         </footer>
     );
