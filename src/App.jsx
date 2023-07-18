10b6b4bbf57790bd54226747b024d25751c59cd4
Nicolas Herrera <herreranicoo17@gmail.com>
2023-07-18 17:36:14 -0300

    Agregue maquetado inicial para crear paciente

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import AdminPacientes from "./components/views/AdminPacientes";
+import CrearPaciente from "./components/views/pacientes/CrearPaciente";
 
 function App() {
   return (
     <>
-      <AdminPacientes />
+      {/* <AdminPacientes /> */}
+      <CrearPaciente />
     </>
   );
 }
