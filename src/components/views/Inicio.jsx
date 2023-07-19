92c94585d1f8993af3bf56c2a1b61bfd7708bbb0
Luciano Angeleri <lcnang@gmail.com>
2023-07-19 01:20:37 -0300

    Se añadió la sección de Servicios con un maquetado básico de Cards

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,11 +1,12 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
+import CardServicio from "./servicio/CardServicio";
 
 const Inicio = () => {
     return (
         <>
             <Container fluid className="text-bg-secondary py-5">
                 <h1 className="display-1 p-3 text-center">Cuida a tus mascotas con RollingVet</h1>
-                <h2 className="h2 px-3  px-md-5 py-2">
+                <h2 className="h2 px-3 px-md-5 py-2">
                     La forma más sencilla de cuidar a tus mascotas. Dales a tus compañeros el cuidado que merecen!
                 </h2>
                 <div className="d-flex justify-content-center gap-2 gap-md-5 p-3">
@@ -13,6 +14,18 @@ const Inicio = () => {
                     <Button variant="outline-primary">Contactanos</Button>
                 </div>
             </Container>
+            <Container className="py-3">
+                <h2 className="display-2 px-3 px-md-5 py-2">
+                    Nuestros Servicios
+                </h2>
+                <Row xs={1} md={2} className="g-3">
+                    <CardServicio></CardServicio>
+                    <CardServicio></CardServicio>
+                    <CardServicio></CardServicio>
+                    <CardServicio></CardServicio>
+                </Row>
+            </Container>
+
         </>
     );
 };
