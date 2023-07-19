517682045992a01cc0e978f13f03219e9400ea46
Luciano Angeleri <lcnang@gmail.com>
2023-07-19 03:12:22 -0300

    Se mejoró el posicionamiento de la image de la card de servicios

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -2,32 +2,29 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import CardServicio from "./servicio/CardServicio";
 
 const Inicio = () => {
-    return (
-        <>
-            <Container fluid className="text-bg-secondary py-5">
-                <h1 className="display-1 p-3 text-center">Cuida a tus mascotas con RollingVet</h1>
-                <h2 className="h2 px-3 px-md-5 py-2">
-                    La forma más sencilla de cuidar a tus mascotas. Dales a tus compañeros el cuidado que merecen!
-                </h2>
-                <div className="d-flex justify-content-center gap-2 gap-md-5 p-3">
-                    <Button variant="primary">Reservar Turno</Button>
-                    <Button variant="outline-primary">Contactanos</Button>
-                </div>
-            </Container>
-            <Container className="py-3">
-                <h2 className="display-2 px-3 px-md-5 py-2">
-                    Nuestros Servicios
-                </h2>
-                <Row xs={1} md={2} className="g-3">
-                    <CardServicio></CardServicio>
-                    <CardServicio></CardServicio>
-                    <CardServicio></CardServicio>
-                    <CardServicio></CardServicio>
-                </Row>
-            </Container>
-
-        </>
-    );
+  return (
+    <>
+      <Container fluid className="text-bg-secondary py-5">
+        <h1 className="display-1 p-3 text-center">Cuida a tus mascotas con RollingVet</h1>
+        <h2 className="h2 px-3 px-md-5 py-2">
+          La forma más sencilla de cuidar a tus mascotas. Dales a tus compañeros el cuidado que merecen!
+        </h2>
+        <div className="d-flex justify-content-center gap-2 gap-md-5 p-3">
+          <Button variant="primary">Reservar Turno</Button>
+          <Button variant="outline-primary">Contactanos</Button>
+        </div>
+      </Container>
+      <Container className="py-3">
+        <h2 className="display-2 px-3 px-md-5 py-2">Nuestros Servicios</h2>
+        <Row xs={1} md={2} className="g-3">
+          <CardServicio></CardServicio>
+          <CardServicio></CardServicio>
+          <CardServicio></CardServicio>
+          <CardServicio></CardServicio>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Inicio;
--- src/components/views/servicio/CardServicio.jsx
+++ src/components/views/servicio/CardServicio.jsx
@@ -2,24 +2,25 @@ import { Card, Col, Button } from "react-bootstrap";
 import imgConsulta from "../../../assets/CardServicio/servicio_Consulta.jpg";
 import "../../../css/CardServicio.css";
 
-
 const CardServicio = () => {
-    return (
-        <Col>
-            <Card className="bg-dark text-white rounded-4">
-                <Card.Img src={imgConsulta} alt="Consultas veterinarias especializadas" className="rounded-4" />
-                <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-                    <Card.Title className="rounded-4 p-2 text-bg-primary">Consultas veterinarias especializadas</Card.Title>
-                    <div className="rounded-4 p-2 text-bg-dark d-flex flex-column flex-lg-row justify-content-between">
-                        <Card.Text className="">
-                            Obtén acceso a consultas veterinarias de calidad con profesionales altamente capacitados.
-                        </Card.Text>
-                        <Button variant="outline-light">Ver Más</Button>
-                    </div>
-                </Card.ImgOverlay>
-            </Card>
-        </Col>
-    );
+  return (
+    <Col>
+      <Card className="rounded-4 overflow-hidden" style={{ height: "30rem"}}>
+        <Card.Img src={imgConsulta} alt="Consultas veterinarias especializadas" className="rounded-4 w-100 h-100 object-fit-cover" />
+        <Card.ImgOverlay className="d-flex flex-column justify-content-end p-2">
+          <div className="w-100 mt-auto">
+            <Card.Title className="rounded-4 p-2 text-bg-primary">Consultas veterinarias especializadas</Card.Title>
+            <div className="rounded-4 p-2 text-bg-dark d-flex flex-column flex-lg-row justify-content-between">
+              <Card.Text className="">
+                Obtén acceso a consultas veterinarias de calidad con profesionales altamente capacitados.
+              </Card.Text>
+              <Button variant="outline-light">Ver Más</Button>
+            </div>
+          </div>
+        </Card.ImgOverlay>
+      </Card>
+    </Col>
+  );
 };
 
 export default CardServicio;
