40bfbf9a33ce74b6d25cd7dffadc0ffef7592b87
estebanrm1 <solsel90@gmail.com>
2023-07-20 02:48:23 -0300

    Agregue la imagen del perrito a la pagina de error 404 y los estilos correspondientes para que se ajuste al tamaño de la pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarPageComponent from './components/common/NavbarComponent';
 import FooterComponent from "./components/common/FooterComponent";
+import Error404 from "./components/views/Error404";
 function App() {
 
   return (
     <>
     {/* <NavbarPageComponent></NavbarPageComponent> */}
+    <Error404></Error404>
     <FooterComponent></FooterComponent>
     </>
   )
--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,9 +1,9 @@
 import '../../css/error404.css'
-
+import imagen404 from '../../assets/ImagenError404.jpg'
 const Error404 = () => {
     return (
         <section className="text-center">
-        
+        <img src={imagen404} className='perrito_error404' alt="Error 404" />
         </section>
     );
 };
