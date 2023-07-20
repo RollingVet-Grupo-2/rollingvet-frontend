ea996c50e18d03fbe2ede294065e6ff2180e5a5b
estebanrm1 <solsel90@gmail.com>
2023-07-20 04:00:43 -0300

    Se agregaron los botones de la pagina de error 404 y sus estilos correspondientes. Tambien modifique los estilos a la imagen del perrito para solucionar problemas de responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarPageComponent from './components/common/NavbarComponent';
 import FooterComponent from "./components/common/FooterComponent";
 import Error404 from "./components/views/Error404";
+import "./App.css";
 function App() {
 
   return (
--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,9 +1,14 @@
 import '../../css/error404.css'
 import imagen404 from '../../assets/ImagenError404.jpg'
+import { Button } from 'react-bootstrap';
 const Error404 = () => {
     return (
-        <section className="text-center">
-        <img src={imagen404} className='perrito_error404' alt="Error 404" />
+        <section className="text-center errorSection">
+        <img src={imagen404} className='perrito_error404 img-fluid' alt="Error 404" />
+        <div className='flex-sm-column mb-5'>
+            <Button className='boton-error404 mx-2'>VOLVER ATRAS</Button>
+            <Button className='boton-error404'>PAGINA DE INICIO</Button>
+        </div>
         </section>
     );
 };
