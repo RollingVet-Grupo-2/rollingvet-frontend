eded6fdb6d570edaa14a48a03ff2d6eb9c652ce5
Luciano Angeleri <lcnang@gmail.com>
2023-07-20 13:25:06 -0300

    Se modificó la card de productos para que reciba datos por props y se cambiaron las imagenes de los productos

--- src/components/views/producto/CardProducto.jsx
+++ src/components/views/producto/CardProducto.jsx
@@ -1,23 +1,20 @@
 import { Card, Col, Button } from "react-bootstrap";
-import imgProducto1 from "../../../assets/CardProducto/producto_Juguete.jpg";
-import imgProducto2 from "../../../assets/CardProducto/producto_Peluche.jpg";
-import imgProducto3 from "../../../assets/CardProducto/producto_Cesto.jpg";
-import imgProducto4 from "../../../assets/CardProducto/producto_Croquetas.jpg";
-import imgProducto5 from "../../../assets/CardProducto/producto_Gato.jpg";
 
-
-const CardProducto = () => {
+const CardProducto = ({nombreProducto, precioProducto, imgProducto}) => {
     return (
         <Col>
-            <Card className="rounded-4 overflow-hidden" style={{ height: "20rem" }}>
-                <Card.Img src={imgProducto1} alt="Producto 1" className="rounded-4 w-100 h-100 object-fit-cover" />
+            <Card className="rounded-4 overflow-hidden" style={{ height: "25rem", backgroundColor:"#F4B693"}}>
+                <Card.Img src={imgProducto} alt={nombreProducto} className="rounded-4 my-auto px-3 object-fit-contain" />
                 <Card.ImgOverlay className="d-flex flex-column justify-content-end p-2">
                     <div className="w-100 mt-auto rounded-4 p-2 text-bg-primary">
-                        <Card.Title>Producto 1</Card.Title>
+                        <Card.Title>{nombreProducto}</Card.Title>
                         <div className="d-flex justify-content-between align-items-center">
+                            <div>
+                            <Card.Subtitle>Datos del producto</Card.Subtitle>
                             <Card.Text>
-                                Datos del producto
+                                Precio: ${precioProducto}
                             </Card.Text>
+                            </div>
                             <Button variant="dark fs-1 fw-bold">+</Button>
                         </div>
                     </div>
--- src/components/views/producto/SliderProducto.jsx
+++ src/components/views/producto/SliderProducto.jsx
@@ -2,6 +2,13 @@ import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { ArrowRight, ArrowLeft } from "react-bootstrap-icons";
 import CardProducto from "./CardProducto";
 
+import imgProducto1 from "../../../assets/CardProducto/producto_Abrigo.png";
+import imgProducto2 from "../../../assets/CardProducto/producto_Cama.png";
+import imgProducto3 from "../../../assets/CardProducto/producto_ComidaRoyalCanin.png";
+import imgProducto4 from "../../../assets/CardProducto/producto_JugueteGato.png";
+import imgProducto5 from "../../../assets/CardProducto/producto_JugueteHuesos.png";
+import imgProducto6 from "../../../assets/CardProducto/producto_Plato.png";
+
 const SliderProducto = () => {
   return (
     <Container>
@@ -16,12 +23,13 @@ const SliderProducto = () => {
           </Button>
         </div>
       </div>
-      <Row xs={2} md={3} lg={4} className="g-3">
-        <CardProducto></CardProducto>
-        <CardProducto></CardProducto>
-        <CardProducto></CardProducto>
-        <CardProducto></CardProducto>
-        <CardProducto></CardProducto>
+      <Row xs={1} md={3} lg={4} className="g-3">
+        <CardProducto nombreProducto={"Producto 1"} precioProducto={1500} imgProducto={imgProducto1}></CardProducto>
+        <CardProducto nombreProducto={"Producto 2"} precioProducto={2000} imgProducto={imgProducto2}></CardProducto>
+        <CardProducto nombreProducto={"Producto 3"} precioProducto={5000} imgProducto={imgProducto3}></CardProducto>
+        <CardProducto nombreProducto={"Producto 4"} precioProducto={4000} imgProducto={imgProducto4}></CardProducto>
+        <CardProducto nombreProducto={"Producto 5"} precioProducto={2401} imgProducto={imgProducto5}></CardProducto>
+        <CardProducto nombreProducto={"Producto 6"} precioProducto={5402} imgProducto={imgProducto6}></CardProducto>
       </Row>
     </Container>
   );
