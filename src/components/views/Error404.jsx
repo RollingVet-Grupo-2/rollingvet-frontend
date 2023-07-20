6059397bf4fdab8bcecb774ad2734ee3fe9e9315
estebanrm1 <solsel90@gmail.com>
2023-07-20 16:23:08 -0300

    Modifique estilos y estructura para acomodar la imagen del perrito en la pagina de error 404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,14 +1,22 @@
 import '../../css/error404.css'
-import imagen404 from '../../assets/imagenErrorRV.jpg'
 import { Button } from 'react-bootstrap';
+import perrito from '../../assets/Dog-Transparent-Background.png';
+
 const Error404 = () => {
     return (
-        <section className="text-center errorSection">
-        <img src={imagen404} className='perrito_error404 img-fluid' alt="Error 404" />
-        <div className='flex-sm-column mb-5'>
-            <Button className='boton-error404 mx-2'>VOLVER ATRAS</Button>
-            <Button className='boton-error404'>PAGINA DE INICIO</Button>
+        <section className="text-center seccionPrincipal">
+        <div>
+            <div className='perrito'><img className='perrito_error404' src={perrito} alt="Error 404" /></div>
+            <div className='contenedor-404 p-5 card-error'>
+            <h3>ERROR 404</h3>
+            <p>No se pudo encontrar la pagina solicitada.</p>
+            <p>Puedes elegir las siguientes opciones:</p>
+            <div className='flex-sm-column mb-5'>
+                <Button className='boton-error404 my-2 my-sm-0 mx-2'>VOLVER ATRAS</Button>
+                <Button className='boton-error404'>PAGINA DE INICIO</Button>
+            </div>
         </div>
+            </div>
         </section>
     );
 };
