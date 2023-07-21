175d45c0fc20c4545d60cd1d6a5703a1af0af9ae
Luciano Angeleri <lcnang@gmail.com>
2023-07-21 19:03:09 -0300

    Se corrigieron las posiciones de las formas en los separadores de secciones

--- src/components/common/SeparadorCustom.jsx
+++ src/components/common/SeparadorCustom.jsx
@@ -4,11 +4,11 @@ import "../../css/Filtros.css";
 const SeparadorCustom = ({ imgSeparador, forma }) => {
     return (
         <Container className="py-3">
+            <div className="w-100 h-100 px-4 position-relative">
+                <img src={forma} alt="Forma de separador" className="w-25 w-25 position-absolute z-n1 fromaSeparador" />
+            </div>
             <div className="w-100 px-5 position-relative">
                 <img src={imgSeparador} alt="Imágen de separador" className="w-25 h-25 filtroGris" />
-            </div>
-            <div className="w-100 h-100 position-relative">
-                <img src={forma} alt="Forma de separador" className="w-25 w-25 position-absolute z-n1 fromaSeparador" />
             </div>
             <hr className="dashed" />
         </Container>
