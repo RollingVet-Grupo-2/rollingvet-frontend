f79adc4eed02e0d2f16bf278c6598aceb069e505
estebanrm1 <solsel90@gmail.com>
2023-07-22 15:14:09 -0300

    Se agrego al componente el formulario y la imagen de ilustracion con sus respectivos estilos y media queries :D

--- src/components/views/FormularioContacto.jsx
+++ src/components/views/FormularioContacto.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
-import { Container, Form } from 'react-bootstrap';
+import '../../css/pagContacto.css'
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import perroFormulario from '../../assets/img/paginaContacto/perroForm.png'
 
 const FormularioContacto = () => {
     return (
-        <Container>
-            <div>
-
-            </div>
-            <Form>
-                
-            </Form>
-        </Container>
+        <section className='my-5 pt-5 d-flex flex-column-reverse flex-md-row contenedor_formulario justify-content-around'>
+            <aside className='col-12 col-md-6 col-lg-5 text-center'>
+                <h3>Nos contactamos con vos!</h3>
+                <p>Complete el formulario con sus datos, deje su mensaje y un agente de atencion al cliente se pondra en contacto con usted. </p>
+                <img className='perro_formulario' src={perroFormulario} alt="" />
+            </aside>
+            <aside className='text-center col-12 col-md-5 col-lg-4 rounded fondo_huellas mb-5'>
+                <h4 className='pt-2'>Formulario de contacto</h4>
+                <hr />
+                <Row className='"mx-0 justify-content-md-center'>
+                    <Col md="auto">
+                        <Form className='text-center'>
+                            <Form.Group>
+                                <Form.Label>
+                                    Nombre y apellido
+                                </Form.Label>
+                                <Form.Control className='bg-secondary border-dark-subtle bg-opacity-50 border-bottom border-0' type='text' required></Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>
+                                    Email
+                                </Form.Label>
+                                <Form.Control className='bg-secondary border-dark-subtle bg-opacity-50 border-bottom border-0' type='email' required></Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>
+                                    Telefono
+                                </Form.Label>
+                                <Form.Control className='bg-secondary border-dark-subtle bg-opacity-50 border-bottom border-0' type='number' required></Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>
+                                    Consulta/Mensaje
+                                </Form.Label>
+                                <Form.Control className='bg-secondary border-dark-subtle bg-opacity-50 border-bottom border-0' as='textarea' type='email' required></Form.Control>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
+            </aside>
+        </section>
     );
 };
 
--- src/components/views/PaginaContacto.jsx
+++ src/components/views/PaginaContacto.jsx
@@ -2,12 +2,13 @@ import { Container } from "react-bootstrap";
 import gatito from '../../assets/img/paginaContacto/pexels-tranmautritam-2188791.jpg'
 import '../../css/pagContacto.css'
 import mascotas from '../../assets/img/paginaContacto/pngegg (3).png'
+import FormularioContacto from "./FormularioContacto";
 
 
 const PaginaContacto = () => {
     return (
+        <>
         <Container className="seccionPrincipal text-center">
-        
             <div className="mascotas col-12">
                 <img className="imagen_mascotas animate__animated animate__fadeInUp" src={mascotas} alt="Imagen mascotas" />
             </div>
@@ -15,13 +16,13 @@ const PaginaContacto = () => {
                 <aside className="col-md-11 col-lg-7 text-center">
                     <div className="card_texto p-lg-5 ms-lg-5 mt-lg-5 animate__animated animate__fadeInUp">
                         <h2>Aquí encontraras diferentes medios para poder contactarte con nosotros</h2>
-                        <hr/>
+                        <hr />
                         <p className="fs-5">En nuestra veterinaria, nos apasiona el cuidado y bienestar de todas las mascotas. Nuestras instalaciones están equipadas con tecnología de vanguardia para diagnosticar y tratar diversas condiciones de salud. En nuestra veterinaria, su compañero peludo será tratado con el mismo cariño y dedicación que brindaríamos a nuestros propios animales.</p>
                         <h5 className="mt-lg-3">Te esperamos en:</h5>
                         <div className="col-12 col-lg-7 card_direccion float-end p-2 animate__animated animate__jackInTheBox">
                             <i className="fi fi-sr-marker"></i>
-                            <p className="pt-2"><b>Gral. Paz 576, T4000 
-                            <br />San Miguel de Tucumán, Tucumán</b></p>
+                            <p className="pt-2"><b>Gral. Paz 576, T4000
+                                <br />San Miguel de Tucumán, Tucumán</b></p>
                         </div>
                     </div>
                 </aside>
@@ -29,9 +30,9 @@ const PaginaContacto = () => {
                     <img className="img-fluid" src={gatito} alt="Imagen gatito" />
                 </aside>
             </article>
-            
-            
         </Container>
+        <FormularioContacto></FormularioContacto>
+        </>
     );
 };
 
