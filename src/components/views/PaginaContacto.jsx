2eafcba26682220b53501781aad77c48f065b9e8
estebanrm1 <solsel90@gmail.com>
2023-07-22 00:26:11 -0300

    Agregue la imagen de gatitos que usare para la pagina de contacto, el texto de descripcion y el archivo para agregar los estilos

--- src/components/views/PaginaContacto.jsx
+++ src/components/views/PaginaContacto.jsx
@@ -1,10 +1,21 @@
+import { Container } from "react-bootstrap";
+import gatito from '../../assets/pexels-arina-krasnikova-7725999.jpg'
 
 
 const PaginaContacto = () => {
     return (
-        <section className="seccionPrincipal">
-            
+        <Container className="seccionPrincipal">
+        <section>
+            <article className="text-center">
+            <p>En nuestra veterinaria, nos apasiona el cuidado y bienestar de todas las mascotas. Nuestro equipo de profesionales altamente capacitados está comprometido con brindar atención médica de primera calidad a perros, gatos y otros animales domésticos. Desde exámenes de rutina hasta tratamientos especializados, nuestro enfoque compasivo garantiza que cada paciente se sienta cómodo y seguro. Nuestras instalaciones están equipadas con tecnología de vanguardia para diagnosticar y tratar diversas condiciones de salud, y nos enorgullece establecer una relación cercana con los dueños, para asegurarnos de entender y satisfacer las necesidades individuales de cada mascota. En nuestra veterinaria, su compañero peludo será tratado con el mismo cariño y dedicación que brindaríamos a nuestros propios animales.</p>
+            </article>
+            <span></span>
+            <div>
+                <img src={gatito} alt="Imagen gatitos" />
+            </div>
         </section>
+            
+        </Container>
     );
 };
 
