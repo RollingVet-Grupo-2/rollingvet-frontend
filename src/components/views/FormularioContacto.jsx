97f52cce392e1be2c1631b010a7dd58c63cd9c54
estebanrm1 <solsel90@gmail.com>
2023-07-22 12:55:32 -0300

    Se creo el componente FormularioContacto y su estructura inicial para maquetar el formulario

---
+++ src/components/views/FormularioContacto.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Form } from 'react-bootstrap';
+
+const FormularioContacto = () => {
+    return (
+        <Container>
+            <div>
+
+            </div>
+            <Form>
+                
+            </Form>
+        </Container>
+    );
+};
+
+export default FormularioContacto;
