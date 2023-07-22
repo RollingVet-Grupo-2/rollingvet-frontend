00ab152313f8c69500601c8fc38b1d981bbb9fe4
estebanrm1 <solsel90@gmail.com>
2023-07-22 03:58:01 -0300

    Agregue los estilos y agregue un link al cdn para las animaciones. Tambien Agregue el contenido de la bienvenida a la pagina

--- src/components/views/PaginaContacto.jsx
+++ src/components/views/PaginaContacto.jsx
@@ -1,21 +1,32 @@
 import { Container } from "react-bootstrap";
 import gatito from '../../assets/pexels-arina-krasnikova-7725999.jpg'
+import '../../css/pagContacto.css'
 
 
 const PaginaContacto = () => {
     return (
-        <Container className="seccionPrincipal">
-        <section>
-            <article className="text-center">
-            <p>En nuestra veterinaria, nos apasiona el cuidado y bienestar de todas las mascotas. Nuestro equipo de profesionales altamente capacitados está comprometido con brindar atención médica de primera calidad a perros, gatos y otros animales domésticos. Desde exámenes de rutina hasta tratamientos especializados, nuestro enfoque compasivo garantiza que cada paciente se sienta cómodo y seguro. Nuestras instalaciones están equipadas con tecnología de vanguardia para diagnosticar y tratar diversas condiciones de salud, y nos enorgullece establecer una relación cercana con los dueños, para asegurarnos de entender y satisfacer las necesidades individuales de cada mascota. En nuestra veterinaria, su compañero peludo será tratado con el mismo cariño y dedicación que brindaríamos a nuestros propios animales.</p>
+        <section className="seccionPrincipal">
+            <article className="d-flex w-100 p-3 contenedor_bienvenida">
+                <article className="text-center">
+                    <div className="card_texto p-5 mt-5 animate__animated animate__fadeInUp">
+                        <h2>Aquí encontraras diferentes medios para poder contactarte con nosotros</h2>
+                        <hr />
+                        <p className="fs-5">En nuestra veterinaria, nos apasiona el cuidado y bienestar de todas las mascotas. Nuestras instalaciones están equipadas con tecnología de vanguardia para diagnosticar y tratar diversas condiciones de salud. En nuestra veterinaria, su compañero peludo será tratado con el mismo cariño y dedicación que brindaríamos a nuestros propios animales.</p>
+                        <h5 className="mt-lg-5">Te esperamos en:</h5>
+                        <div className="card_direccion float-end p-2 animate__animated animate__jackInTheBox">
+                            <i className="fi fi-sr-marker"></i>
+                            <p className="pt-2">Gral. Paz 576, T4000 San Miguel de Tucumán, Tucumán</p>
+                        </div>
+                    </div>
+                </article>
+                {/*<aside className="col_verde">
+                    <div className="linea_vertical"></div>
+    </aside>*/}
+                <div className="contenedor_gatitos animate__animated animate__fadeInUp">
+                    <img className="imagen_gatitos" src={gatito} alt="Imagen gatitos" />
+                </div>
             </article>
-            <span></span>
-            <div>
-                <img src={gatito} alt="Imagen gatitos" />
-            </div>
         </section>
-            
-        </Container>
     );
 };
 
