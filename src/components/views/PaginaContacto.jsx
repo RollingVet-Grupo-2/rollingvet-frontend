d308387e5aa1dc806f1bc21c574619dc8d3a3b05
estebanrm1 <solsel90@gmail.com>
2023-07-26 10:25:41 -0300

    Se agrego margen al texto de descripcion y se agrandaron los iconos

--- src/components/views/PaginaContacto.jsx
+++ src/components/views/PaginaContacto.jsx
@@ -12,17 +12,17 @@ const PaginaContacto = () => {
                 <div className="row col-12 mascotas">
                     <img className="imagen_mascotas animate__animated animate__fadeInUp" src={mascotas} alt="Imagen mascotas" />
                 </div>
-                <article className="row justify-content-center contenedor_bienvenida py-5 py-md-5">
-                    <aside className="col-md-11 col-lg-7 text-center py-4 px-0">
-                        <div className="card_texto p-lg-5 ms-lg-3 mt-md-4 animate__animated animate__fadeInUp">
+                <article className="row justify-content-center contenedor_bienvenida py-5 ps-md-1 py-md-5">
+                    <aside className="col-md-11 col-lg-7 text-center py-4 px-2">
+                        <div className="card_texto py-lg-5 mt-md-4 px-3 animate__animated animate__fadeInUp">
                             <h2>Aquí encontraras diferentes medios para poder contactarte con nosotros</h2>
                             <hr />
                             <p className="fs-4 fw-semibold">En nuestra veterinaria, nos apasiona el cuidado y bienestar de todas las mascotas. Nuestras instalaciones están equipadas con tecnología de vanguardia para diagnosticar y tratar diversas condiciones de salud. En nuestra veterinaria, su compañero peludo será tratado con el mismo cariño y dedicación que brindaríamos a nuestros propios animales.</p>
                             <hr/>
                             <h5 className="my-lg-4">Te esperamos en:</h5>
-                            <div className="col-12 col-lg-7 card_direccion float-end p-3 animate__animated animate__jackInTheBox">
-                                <i className="fi fi-sr-marker"></i>
-                                <p className="pt-4"><b>Gral. Paz 576, T4000
+                            <div className="py-4 col-12 col-lg-7 card_direccion float-end p-3 animate__animated animate__jackInTheBox">
+                                <i className="fs-4 fi fi-sr-marker"></i>
+                                <p><b>Gral. Paz 576, T4000
                                     <br />San Miguel de Tucumán, Tucumán</b></p>
                             </div>
                         </div>
@@ -37,15 +37,15 @@ const PaginaContacto = () => {
                 <article className="container row">
                     <h3 className="mb-5">Estamos disponibles las 24 horas, todos los días.</h3>
                     <div className="col-md-4">
-                    <i className="bi bi-envelope-at"></i>
+                    <i className="fs-4 bi bi-envelope-at"></i>
                         <p>rolling_vet@rolling.com</p>
                     </div>
                     <div className="col-md-4 my-5 my-md-0">
-                    <i className="bi bi-instagram"></i>
+                    <i className="fs-4 bi bi-instagram"></i>
                         <p>@rolling_vet</p>
                     </div>
                     <div className="col-md-4">
-                    <i className="bi bi-telephone-outbound"></i>
+                    <i className="fs-4 bi bi-telephone-outbound"></i>
                         <p>381-6543210</p>
                     </div>
                 </article>
