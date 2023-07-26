752b1d37fd5223efeebc23d5f51bde51cdd0ce4f
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-26 17:51:11 -0300

    Se importaron los componentes BrowserRouter Routes y Route en App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import NavbarPageComponent from './components/common/NavbarComponent';
 import FooterComponent from "./components/common/FooterComponent";
 import Error404 from "./components/views/Error404";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
     <NavbarPageComponent></NavbarPageComponent>
-    {/*    <Error404></Error404> */}
+      <Routes>
+        <Route path="/error404" element={<Error404></Error404>}></Route>
+      </Routes>
     <FooterComponent></FooterComponent>
-    </>
+    </BrowserRouter>
   )
 }
 
