ff7aa7ba7e37469fbfdbc3c198ee7dbf92e2173b
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-30 02:07:22 -0300

    Rutas del administrador enviadas al componenre RutasAdministrador

--- src/components/routes/RutasAdministrador.jsx
+++ src/components/routes/RutasAdministrador.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import { Routes, Route } from "react-router";
+import AdminPrincipal from "../views/AdminPrincipal";
 const RutasAdministrador = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Routes>
+                <Route
+                    exact
+                    path="/"
+                    element={<AdminPrincipal></AdminPrincipal>}
+                ></Route>
+                <Route
+                    exact
+                    path="/crear-turno"
+                    element={<CrearTurno></CrearTurno>}
+                ></Route>
+                <Route
+                    exact
+                    path="/editar-turno/:id"
+                    element={<EditarTurno></EditarTurno>}
+                ></Route>
+            </Routes>
+        </>
     );
 };
 
