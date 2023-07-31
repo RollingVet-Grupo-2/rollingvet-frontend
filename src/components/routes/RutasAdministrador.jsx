579394806790e13cdaed09a8f653083bbd172199
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-30 23:39:36 -0300

    Agregamos las rutas que faltaban del admin a RutasAdministrador

--- src/components/routes/RutasAdministrador.jsx
+++ src/components/routes/RutasAdministrador.jsx
@@ -5,6 +5,8 @@ import EditarTurno from "../views/turnos/EditarTurno";
 import AdminPacientes from "../views/AdminPacientes";
 import AdminTurnos from "../views/AdminTurnos";
 import Error404 from "../views/Error404";
+import CrearPaciente from "../views/pacientes/CrearPaciente";
+import EditarPaciente from "../views/pacientes/EditarPaciente";
 const RutasAdministrador = () => {
     return (
         <>
@@ -16,12 +18,12 @@ const RutasAdministrador = () => {
                 ></Route>
                 <Route
                     exact
-                    path="/administar-pacientes"
+                    path="/pacientes"
                     element={<AdminPacientes></AdminPacientes>}
                 ></Route>
                 <Route
                     exact
-                    path="/administar-turnos"
+                    path="/turnos"
                     element={<AdminTurnos></AdminTurnos>}
                 ></Route>
                 <Route
@@ -33,6 +35,16 @@ const RutasAdministrador = () => {
                     exact
                     path="/editar-turno/:id"
                     element={<EditarTurno></EditarTurno>}
+                ></Route>
+                <Route
+                    exact
+                    path="/crear-paciente"
+                    element={<CrearPaciente></CrearPaciente>}
+                ></Route>
+                <Route
+                    exact
+                    path="/editar-paciente/:id"
+                    element={<EditarPaciente></EditarPaciente>}
                 ></Route>
                 <Route path="*" element={<Error404></Error404>}></Route>
             </Routes>
