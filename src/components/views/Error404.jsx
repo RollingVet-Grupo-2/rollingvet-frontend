118786cc62423faedf2406cf8283f284b0ea5ce6
estebanrm1 <solsel90@gmail.com>
2023-08-09 12:35:14 -0300

    Cambie colores de pagina 404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -11,12 +11,12 @@ const Error404 = () => {
     const irAlInicio = () => navigate("/");
 
     return (
-        <section className="text-center seccionPrincipal">
+        <section className="text-center seccionPrincipal bg-secondary">
             <div>
                 <div className='perrito'><img className='perrito_error404' src={perrito} alt="Error 404" /></div>
                 <div className='contenedor-404 p-5 card-error'>
-                    <h3>ERROR 404</h3>
-                    <p>No se pudo encontrar la pagina solicitada.</p>
+                    <h2>ERROR 404</h2>
+                    <p className='f-2'>No se pudo encontrar la pagina solicitada.</p>
                     <p>Puedes elegir las siguientes opciones:</p>
                     <div className='flex-sm-column mb-5'>
                         <Button className='boton-error404 my-2 my-sm-0 mx-2' onClick={volverAtras}><b>VOLVER ATRAS</b></Button>
