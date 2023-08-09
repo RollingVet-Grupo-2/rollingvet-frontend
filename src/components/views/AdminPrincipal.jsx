0fea8a1528687344ec9d3c5bca7f034845cf6e47
Luciano Angeleri <lcnang@gmail.com>
2023-08-09 03:25:51 -0300

    Se maquetó los elementos principales del administrador de servicios

--- src/components/views/AdminPrincipal.jsx
+++ src/components/views/AdminPrincipal.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row, Table } from "react-bootstrap";
+import { Button, Card, Col, Container, Row, Table } from "react-bootstrap";
 import trabajadores from "../../assets/img/administrador/co-workers.svg";
 import vets from "../../assets/img/administrador/vets.svg";
 import "../../css/AdminPrincipal.css";
@@ -78,58 +78,56 @@ const AdminPrincipal = () => {
   };
 
   return (
-    <section className="container text-center py-3">
-      <section className="border border-3 rounded rounded-3 shadow mb-3">
-        <Row className="row-cols-1 row-cols-md-2 justify-content-center align-items-center">
+    <Container className="container text-center py-3">
+      <Container className="border border-4  rounded-4 border-dark rounded rounded-3 shadow mb-3">
+        <Row className="row-cols-1 row-cols-md-2 justify-content-center align-items-center g-3">
           <Col>
-            <h1 className="mb-0">¡Hola, Admin!</h1>
-            <p className="lead mb-0">
-              Te damos la bienvenida a la página principal de administración.
-            </p>
+            <h1 className=" display-1 fs-2">¡Hola, Admin!</h1>
+            <p className="lead">Te damos la bienvenida a la página principal de administración.</p>
           </Col>
           <Col>
             <img src={trabajadores} alt="Trabajadores" className="img-fluid" />
           </Col>
         </Row>
-      </section>
-      <section className="py-3">
+      </Container>
+      <Container className="py-3">
         <Row className="row-cols-1 row-cols-md-2 justify-content-center align-items-center">
           <Col>
             <img src={vets} alt="Veterinarios" className="img-fluid" />
           </Col>
           <Col>
-            <h2>
-              Administrá fichas de pacientes y turnos desde cualquier lugar.
-            </h2>
-            <Link to={"pacientes"}>
-              <Button
-                variant="success"
-                size="lg"
-                className="mb-3 mb-md-3 mb-lg-0 me-lg-3 p-3"
-              >
-                Administrar Pacientes
-              </Button>
-            </Link>
-            <Link to={"turnos"}>
-              <Button variant="success" size="lg" className="p-3">
-                Administrar Turnos
-              </Button>
-            </Link>
+            <h2 className="display- py-3">Administrador</h2>
+            <p className="lead py-3">Administrá fichas de <b>pacientes</b>, <b>turnos</b> y <b>servicios</b> desde cualquier lugar.</p>
+            <div className="d-flex flex-column flex-lg-row gap-3">
+              <Link to={"pacientes"}>
+                <Button variant="primary" size="lg" className="p-3 rounded-4 w-100">
+                  Administrar <b>PACIENTES</b>
+                </Button>
+              </Link>
+              <Link to={"turnos"}>
+                <Button variant="primary" size="lg" className="p-3 rounded-4 w-100">
+                  Administrar <b>TRUNOS</b>
+                </Button>
+              </Link>
+              <Link to={"servicios"}>
+                <Button variant="primary" size="lg" className="p-3 rounded-4 w-100">
+                  Administrar <b>SERVICIOS</b>
+                </Button>
+              </Link>
+            </div>
           </Col>
         </Row>
-      </section>
+      </Container>
       <hr />
-      <section className="row justify-content-center align-items-center pt-3">
-        <h2 className="py-3">
-          Visualizá rapidamente y de forma sencilla la cantidad de veterinarios
-          disponibles, pacientes registrados y turnos asignados.
+      <Container className="row justify-content-center align-items-center pt-3">
+        <h2 className="display-3 py-3">
+          Resumen
         </h2>
-        <Card
-          bg="info"
-          text="light"
-          style={{ width: "18rem" }}
-          className="mb-3 me-md-3 shadow py-2"
-        >
+        <p className="lead py-3">
+          Visualizá rapidamente y de forma sencilla la cantidad de veterinarios disponibles, pacientes registrados y
+          turnos asignados.
+        </p>
+        <Card bg="info" text="dark" style={{ width: "18rem" }} className="border-3 rounded-4 mb-3 me-md-3 shadow py-2">
           <Card.Body as={Row} className="align-items-center">
             <Col xs={2}>
               <span>
@@ -143,12 +141,7 @@ const AdminPrincipal = () => {
           </Card.Body>
         </Card>
 
-        <Card
-          bg="success"
-          text="light"
-          style={{ width: "18rem" }}
-          className="mb-3 me-md-3 shadow py-2"
-        >
+        <Card bg="success" text="dark" style={{ width: "18rem" }} className="border-3 rounded-4 mb-3 me-md-3 shadow py-2">
           <Card.Body as={Row} className="align-items-center">
             <Col xs={2}>
               <span>
@@ -158,20 +151,13 @@ const AdminPrincipal = () => {
             <Col xs={10}>
               <Card.Title>Veterinarios</Card.Title>
               <Card.Text className="fw-bold">
-                {veterinarios
-                  ? veterinarios.length
-                  : "No se pudo obtener datos"}
+                {veterinarios ? veterinarios.length : "No se pudo obtener datos"}
               </Card.Text>
             </Col>
           </Card.Body>
         </Card>
 
-        <Card
-          bg="danger"
-          text="light"
-          style={{ width: "18rem" }}
-          className="mb-3 me-md-3 shadow py-2"
-        >
+        <Card bg="danger" text="dark" style={{ width: "18rem" }} className="border-3 rounded-4 mb-3 me-md-3 shadow py-2">
           <Card.Body as={Row} className="align-items-center">
             <Col xs={2}>
               <span>
@@ -187,12 +173,7 @@ const AdminPrincipal = () => {
           </Card.Body>
         </Card>
 
-        <Card
-          bg="warning"
-          text="light"
-          style={{ width: "18rem" }}
-          className="mb-3 me-md-3 shadow py-2"
-        >
+        <Card bg="warning" text="dark" style={{ width: "18rem" }} className="border-3 rounded-4 mb-3 me-md-3 shadow py-2">
           <Card.Body as={Row} className="align-items-center">
             <Col xs={2}>
               <span>
@@ -205,17 +186,24 @@ const AdminPrincipal = () => {
             </Col>
           </Card.Body>
         </Card>
-      </section>
-      <section className="py-3">
-        <h3>Lista de Turnos</h3>
-        <Table
-          striped
-          responsive
-          bordered
-          hover
-          variant="dark"
-          className="table table-center"
-        >
+        
+        <Card bg="primary" text="dark" style={{ width: "18rem" }} className="border-3 rounded-4 mb-3 me-md-3 shadow py-2">
+          <Card.Body as={Row} className="align-items-center">
+            <Col xs={2}>
+              <span>
+                <i className="bi bi-file-text fs-1"></i>
+              </span>
+            </Col>
+            <Col xs={10}>
+              <Card.Title>Servicios</Card.Title>
+              <Card.Text className="fw-bold">4</Card.Text>
+            </Col>
+          </Card.Body>
+        </Card>
+      </Container>
+      <Container className="py-3">
+        <h3 className="display-3">Lista de Turnos</h3>
+        <Table striped responsive bordered hover variant="dark" className="table table-center">
           <thead>
             <tr>
               <th>Veterinario</th>
@@ -232,15 +220,13 @@ const AdminPrincipal = () => {
                 </td>
               </tr>
             ) : (
-              turnos.map((turno) => (
-                <TurnosAsignados key={turno._id} turno={turno} />
-              ))
+              turnos.map((turno) => <TurnosAsignados key={turno._id} turno={turno} />)
             )}
           </tbody>
         </Table>
         <hr />
-      </section>
-    </section>
+      </Container>
+    </Container>
   );
 };
 
