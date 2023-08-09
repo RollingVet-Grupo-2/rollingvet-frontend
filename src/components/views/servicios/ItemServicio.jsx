cd204f720497a5daee7e9826cc43a2741ea5625d
Luciano Angeleri <lcnang@gmail.com>
2023-08-09 18:19:34 -0300

    Se agregaron las funciones para leer, editar y borrar los datos de los servicios en la db.json

--- src/components/views/servicios/ItemServicio.jsx
+++ src/components/views/servicios/ItemServicio.jsx
@@ -7,7 +7,7 @@ const ItemServicio = ({ servicio }) => {
       <td>{servicio.nombre}</td>
       <td>{servicio.veterinario}</td>
       <td><img src={servicio.img} alt={servicio.nombre} className="img-thumbnail rounded-4" style={{ maxWidth: "5rem" }}/></td>
-      <td>${servicio.precio}</td>
+      <td>$ {servicio.precio}</td>
       <td className="text-truncate" style={{ maxWidth: "16rem" }}>{servicio.descripcion}</td>
       <td>
         <div className="d-flex gap-2 justify-content-center">
